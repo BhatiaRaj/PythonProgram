68b07967325814ad075438c7e2f3b300d0f3edad
BhatiaRaj <97431540+BhatiaRaj@users.noreply.github.com>
2022-01-17 19:48:12 +0530

    Create Program-3.py
    Take a list, a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], and write a program that print out all the elements of the list that are less than 5.

---
+++ Program-3.py
@@ -0,0 +1,10 @@
+# Take a list, a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], and write a program that print out all the elements of the list that are less than 5.
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for i in a:
+
+    if i < 5:
+
+        print(i)
