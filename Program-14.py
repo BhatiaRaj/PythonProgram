f0c132dd9ca5314f8804717b3e60ac03a393f783
BhatiaRaj <97431540+BhatiaRaj@users.noreply.github.com>
2022-01-27 16:48:45 +0530

    Create Program-14.py
    Simple Calculator
    # Write a program to take two integers as input and output their sum.

---
+++ Program-14.py
@@ -0,0 +1,13 @@
+# Simple Calculator
+# Write a program to take two integers as input and output their sum.
+# Sample Input:
+# 2
+# 8
+# Sample Output:   10
+
+# code goes here
+num1 = int(input())
+num2 = int(input())
+
+sum = int(num1 + num2)
+print(sum)
