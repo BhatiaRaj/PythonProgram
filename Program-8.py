29ffdca61af02ffcd11171d513c7fee6881e5f11
BhatiaRaj <97431540+BhatiaRaj@users.noreply.github.com>
2022-02-13 10:12:04 +0530

    Update Program-8.py

--- Program-8.py
+++ Program-8.py
@@ -2,10 +2,10 @@
 
 import sys
 
-user1 = input("What's your name?")
-user2 = input("And your name?")
-user1_answer = input("%s, do yo want to choose rock, paper or scissors?" % user1)
-user2_answer = input("%s, do you want to choose rock, paper or scissors?" % user2)
+user1 = input("What's your name?\n")
+user2 = input("And your name?\n")
+user1_answer = input("%s, do yo want to choose rock, paper or scissors?\n" % user1)
+user2_answer = input("%s, do you want to choose rock, paper or scissors?\n" % user2)
 
 def compare(u1, u2):
     if u1 == u2:
